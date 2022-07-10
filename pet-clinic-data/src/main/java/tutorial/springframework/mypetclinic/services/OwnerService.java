4e4da7e9abdb89607f9978242cc063104306a15f
auoia <ptaototo123@gmail.com>
2022-07-09 20:19:52 -0700

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/tutorial/springframework/mypetclinic/services/OwnerService.java
@@ -0,0 +1,15 @@
+package tutorial.springframework.mypetclinic.services;
+
+import tutorial.springframework.mypetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/tutorial/springframework/mypetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package tutorial.springframework.mypetclinic.services;
+
+ import tutorial.springframework.mypetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
