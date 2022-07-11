a1ec33739b8406d7d3c4d92c82f354a309a7d316
auoia <ptaototo123@gmail.com>
2022-07-11 00:51:52 -0700

    reactor service interfaces to common base interface. Closes #11

--- pet-clinic-data/src/main/java/tutorial/springframework/mypetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/tutorial/springframework/mypetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package tutorial.springframework.mypetclinic.services;
 
 import tutorial.springframework.mypetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
